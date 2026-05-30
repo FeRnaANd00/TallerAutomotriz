6af729f0f0418ef9171ac2a1251f42da378d3e36
Fernando <barrientosfernando22@icloud.com>
2026-05-29 22:17:26 -0600

    nuevos datos

---
+++ TallerMecanico.BL/MecanicoBL.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using TallerMecanico.DAL;
+using TallerMecanico.EN;
+
+namespace TallerMecanico.BL
+{
+    public class MecanicoBL
+    {
+        // AGREGAR
+        public static int AgregarMecanico(Mecanico mecanico)
+        {
+            return MecanicoDAL.AgregarMecanico(mecanico);
+        }
+
+        // MOSTRAR
+        public static List<Mecanico> MostrarMecanicos()
+        {
+            return MecanicoDAL.MostrarMecanicos();
+        }
+
+        // MODIFICAR
+        public static int ModificarMecanico(Mecanico mecanico)
+        {
+            return MecanicoDAL.ModificarMecanico(mecanico);
+        }
+
+        // ELIMINAR
+        public static int EliminarMecanico(int id)
+        {
+            return MecanicoDAL.EliminarMecanico(id);
+        }
+
+        // OBTENER POR ID
+        public static Mecanico ObtenerMecanicoPorId(int id)
+        {
+            return MecanicoDAL.ObtenerMecanicoPorId(id);
+        }
+
+        // BUSCAR
+        public static List<Mecanico> BuscarMecanico(string texto)
+        {
+            return MecanicoDAL.BuscarMecanico(texto);
+        }
+    }
+}
---
+++ TallerMecanico.UI/Controllers/MecanicoController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using TallerMecanico.BL;
+using TallerMecanico.EN;
+
+namespace TallerMecanico.UI.Controllers
+{
+    public class MecanicoController : Controller
+    {
+        // LISTAR
+        public IActionResult Index()
+        {
+            var lista =
+                MecanicoBL.MostrarMecanicos();
+
+            return View(lista);
+        }
+
+        // FORMULARIO
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        // GUARDAR
+        [HttpPost]
+        public IActionResult Agregar(Mecanico mecanico)
+        {
+            MecanicoBL.AgregarMecanico(mecanico);
+
+            return RedirectToAction("Index");
+        }
+        // MOSTRAR FORMULARIO EDITAR
+        public IActionResult Editar(int id)
+        {
+            var mecanico =
+                MecanicoBL.ObtenerMecanicoPorId(id);
+
+            return View(mecanico);
+        }
+
+        // GUARDAR EDICION
+        [HttpPost]
+        public IActionResult Editar(Mecanico mecanico)
+        {
+            MecanicoBL.ModificarMecanico(mecanico);
+
+            return RedirectToAction("Index");
+        }
+
+        // MOSTRAR ELIMINAR
+        public IActionResult Eliminar(int id)
+        {
+            var mecanico =
+                MecanicoBL.ObtenerMecanicoPorId(id);
+
+            return View(mecanico);
+        }
+
+        // CONFIRMAR ELIMINAR
+        [HttpPost]
+        public IActionResult Eliminar(Mecanico mecanico)
+        {
+            MecanicoBL.EliminarMecanico(mecanico.Id_mecanico);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
